6b0fc25d024f50c27218ffb1d016313ad990a672
Shay Zaltsman <Shay@native.local>
2017-12-29 11:01:32 +0200

    first draft

---
+++ ftpCourtTransfer_New/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace ftpCourtTransfer_New
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
+            this.copyTo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fromDate = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.toDate = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.startAt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(-5, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(899, 257);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(753, 299);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "העתקה לספריה";
+            // 
+            // copyTo
+            // 
+            this.copyTo.Location = new System.Drawing.Point(575, 296);
+            this.copyTo.Name = "copyTo";
+            this.copyTo.Size = new System.Drawing.Size(161, 20);
+            this.copyTo.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(753, 329);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "מתאריך או שם ספריה";
+            // 
+            // fromDate
+            // 
+            this.fromDate.Location = new System.Drawing.Point(575, 329);
+            this.fromDate.Name = "fromDate";
+            this.fromDate.Size = new System.Drawing.Size(161, 20);
+            this.fromDate.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(413, 331);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "עד תאריך";
+            // 
+            // toDate
+            // 
+            this.toDate.Location = new System.Drawing.Point(247, 329);
+            this.toDate.Name = "toDate";
+            this.toDate.Size = new System.Drawing.Size(160, 20);
+            this.toDate.TabIndex = 6;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(822, 357);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(753, 358);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "התחל ב";
+            // 
+            // startAt
+            // 
+            this.startAt.Location = new System.Drawing.Point(575, 358);
+            this.startAt.Name = "startAt";
+            this.startAt.Size = new System.Drawing.Size(161, 20);
+            this.startAt.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(484, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "לחץ כאן להעתקת קבצים";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(247, 401);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(187, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "לחץ כאן להשוואה  בין ספריות";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 440);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.startAt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.toDate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.fromDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.copyTo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "הורדת קבצים מאתר בתי המשפט";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox copyTo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox fromDate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox toDate;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox startAt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ftpCourtTransfer_New/Form1.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Globalization;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace ftpCourtTransfer_New
+{
+    public partial class Form1 : Form
+    {
+        private string[] arFolderNames;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            copyTo.Text = "c:\\users\\shay";
+            DateTime dtYesterday = DateTime.Now.AddDays(-1);
+            string sYesterday = dtYesterday.Day+"/"+ dtYesterday.Month+"/"+ dtYesterday.Year;
+            fromDate.Text = sYesterday;
+            toDate.Text = sYesterday;
+
+            webBrowser1.Navigate ("https://decisions.court.gov.il");
+            
+        }
+        private void DownloadFile(string sLocalFilePath, string sRemoteFileNm)
+        {
+            //            string inputfilepath = @"C:\tmp1234.docx";
+            //          string httpsfilepath = "/2017-12-23//0012517460010000090037f69315425b.docx";
+            
+            string sHttpsHost = "decisions.court.gov.il";
+
+            string sRemoteFilePath= "https://" + sHttpsHost + sRemoteFileNm;
+
+            using (WebClient request = new WebClient())
+            {
+                request.Credentials = new NetworkCredential("idc\\data-hok", "L1150508a");
+                byte[] fileData = request.DownloadData(sRemoteFilePath);
+                using (FileStream file = File.Create(sLocalFilePath))
+                {
+                    file.Write(fileData, 0, fileData.Length);
+                    file.Close();
+                }
+            }
+        }
+        private bool chkFldsValidity()
+        {
+            bool bValid = true;
+            if (checkBox1.Checked && !IsDate(startAt.Text))
+            {
+                MessageBox.Show("עליך להזין תאריך ושעת התחלה לפי הפורמט המודגם");
+                startAt.Focus();
+                bValid = false;
+            }
+            else if (copyTo.Text == "")
+            {
+                MessageBox.Show("השדה העתקה לספריה ריק");
+                copyTo.Focus();
+                bValid = false;
+            }
+            else if (!Directory.Exists(copyTo.Text))
+            {
+                MessageBox.Show("ספריית היעד אינה קיימת");
+                copyTo.Focus();
+                bValid = false;
+            }
+            else if (fromDate.Text == "")
+            {
+                MessageBox.Show("שדה מתאריך ריק");
+                copyTo.Focus();
+                bValid = false;
+            }
+            else if (toDate.Text == "")
+            {
+                MessageBox.Show("שדה עד תאריך ריק");
+                copyTo.Focus();
+                bValid = false;
+            }
+
+            return bValid;
+            /*
+                ElseIf Trim(fromDate.Text) = "" Then
+                    MsgBox ("äùãä 'îúàøéê' àéððå éëåì ìäéåú øé÷. òìéå ìäëéì àå îçøåæú áôåøîè úàøéê àå îçøåæú àçøú")
+                    fromDate.SetFocus
+                    chkFldsValidity = False
+                ElseIf DateDiff("d", CDate(fromDate.Text), Date) < 1 And _
+                        (Check1.Value = 0 Or DateDiff("d", CDate(fromDate.Text), CDate(startAt.Text)) < 1) Then
+                    MsgBox ("òì äúàøéê áùãä 'îúàøéê' ìäéåú îå÷ãí îï äúàøéê ùì äéåí")
+                    fromDate.SetFocus
+                    chkFldsValidity = False
+                ElseIf DateDiff("d", CDate(toDate.Text), Date) < 1 And _
+                        (Check1.Value = 0 Or DateDiff("d", CDate(toDate.Text), CDate(startAt.Text)) < 1) Then
+                    MsgBox ("òì äúàøéê áùãä 'òã úàøéê' ìäéåú îå÷ãí îï äúàøéê ùì äéåí")
+                    toDate.SetFocus
+                    chkFldsValidity = False
+                ElseIf DateDiff("d", CDate(fromDate.Text), CDate(toDate.Text)) < 0 Then
+                    MsgBox ("'äúàøéê áùãä 'òã úàøéê' îå÷ãí éåúø îï äúàøéê áùãä 'îúàøéê")
+                    fromDate.SetFocus
+                    chkFldsValidity = False
+                End If
+
+
+
+                         */
+
+
+
+
+
+        }
+
+        public static bool IsDate(Object obj)
+        {
+            string strDate = obj.ToString();
+            try
+            {
+                DateTime dt = DateTime.ParseExact(strDate, "dd/MM/yyyy",CultureInfo.InvariantCulture);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (chkFldsValidity())
+            {
+                bool bContinue = true;
+                string copyToFolder = copyTo.Text;
+                string sFolderNames = "";
+                if (copyToFolder.Substring(copyToFolder.Length-1,1)!="/" && copyToFolder.Substring(copyToFolder.Length - 1, 1) != "\\")
+                {
+                    copyToFolder += "\\";
+                }
+                if (IsDate(fromDate.Text) && IsDate(toDate.Text))
+                {
+                    DateTime dtFromDate = DateTime.ParseExact(fromDate.Text, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                    DateTime dtToDate = DateTime.ParseExact(toDate.Text, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                    while (((TimeSpan)(dtToDate - dtFromDate)).Days >= 0)
+                    {
+                        string sFolderName = dtFromDate.Year + "-" + dtFromDate.Month + "-" + dtFromDate.Day;
+                        if (sFolderNames != "") sFolderNames += ",";
+                        sFolderNames += sFolderName;
+                        sFolderName = copyToFolder + sFolderName;
+                        dtFromDate=dtFromDate.AddDays(1);
+                        Directory.CreateDirectory(sFolderName);
+                    }
+                }
+                else if (!IsDate(fromDate.Text) && fromDate.Text != "")
+                {
+                    string sFolderName = copyToFolder + fromDate.Text;
+                    Directory.CreateDirectory(sFolderName);
+                    sFolderNames = sFolderName;
+                }
+                else
+                {
+                    bContinue = false;
+                }
+
+
+
+                /*
+                                ElseIf Not IsDate(fromDate.Text) And fromDate.Text <> "" Then
+                                      tmpFolderName = copyToFolder + fromDate.Text
+                            fs.CreateFolder(tmpFolderName)
+                            DateStr = fromDate.Text
+                        Else
+                            toCont = False
+                        End If
+                */
+                if (bContinue)
+                {
+                    arFolderNames = sFolderNames.Split(',');
+                    foreach (string sFolder in arFolderNames)
+                    {
+                        string tmpUrl = "https://decisions.court.gov.il/" + arFolderNames[0] + "/";
+                        webBrowser1.Navigate(tmpUrl);
+                        while (webBrowser1.IsBusy || webBrowser1.ReadyState != WebBrowserReadyState.Complete)
+                        {
+                            Application.DoEvents();
+                            Application.DoEvents();
+                            Application.DoEvents();
+                            //Thread.Sleep(1000);
+                        }
+                        downloadFiles(sFolder);
+                    }
+                    MessageBox.Show("תהליך ההורדה הסתיים");
+                }
+
+
+            }
+            else
+            {
+            }
+        }
+        private void downloadFiles(string sFolderNm)
+        {
+            string tmpTxt = webBrowser1.Document.Body.InnerHtml;
+            Regex re = new Regex("<a.*?</a>",RegexOptions.IgnoreCase);
+            Regex re2 = new Regex(@"href=""(.*?)""", RegexOptions.IgnoreCase);
+            Regex re3 = new Regex(">(.*?)<", RegexOptions.IgnoreCase);
+            MatchCollection mt = re.Matches(tmpTxt);
+            foreach (Match m in mt)
+            {
+                string sLink = m.ToString();
+                if (sLink.IndexOf("href") > -1 && (sLink.IndexOf("doc")>-1 || sLink.IndexOf("xml") > -1))
+                {
+                    string sHref = re2.Match(sLink).Groups[1].ToString();
+                    string sName = re3.Match(sLink).Groups[1].ToString();
+                    string sLocalPath = copyTo.Text + "\\"+sFolderNm+"\\"+sName;
+                    DownloadFile(sLocalPath, sHref);
+                    Thread.Sleep(1000);
+                }
+            }
+            
+
+            
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool bContinue = true;
+            string sRemoteFolderNm="",sLocalFolderNm="",sLocalFolderPath="";
+
+            if (IsDate(fromDate.Text) && IsDate(toDate.Text))
+            {
+                DateTime dtFromDate = DateTime.ParseExact(fromDate.Text, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                DateTime dtToDate = DateTime.ParseExact(toDate.Text, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                if (((TimeSpan)(dtToDate - dtFromDate)).Days != 0)
+                {
+                    bContinue = false;
+                }
+                else
+                {
+                    sRemoteFolderNm = dtFromDate.Year + "-" + dtFromDate.Month + "-" + dtFromDate.Day;
+                    sLocalFolderNm = sRemoteFolderNm;
+                }
+
+            }
+            else
+            {
+                sRemoteFolderNm = fromDate.Text;
+                sLocalFolderNm = sRemoteFolderNm;
+            }
+
+            sLocalFolderPath = copyTo.Text + "//" + sLocalFolderNm+"//";
+
+            if (!Directory.Exists(sLocalFolderPath))
+            {
+                bContinue=false;
+            }
+            else if (chkFldsValidity() && bContinue)
+            {
+
+                string tmpUrl = "https://decisions.court.gov.il/" + sRemoteFolderNm + "/";
+                bool bAllFilesDownloaded = true;
+                webBrowser1.Navigate(tmpUrl);
+                while (webBrowser1.IsBusy || webBrowser1.ReadyState != WebBrowserReadyState.Complete)
+                {
+                    Application.DoEvents();
+                    Application.DoEvents();
+                    Application.DoEvents();
+                    //Thread.Sleep(1000);
+                }
+
+
+                string tmpTxt = webBrowser1.Document.Body.InnerHtml;
+                List<string> arFiles = new List<string>(); 
+                Regex re = new Regex("<a.*?</a>", RegexOptions.IgnoreCase);
+                Regex re2 = new Regex(@"href=""(.*?)""", RegexOptions.IgnoreCase);
+                Regex re3 = new Regex(">(.*?)<", RegexOptions.IgnoreCase);
+                MatchCollection mt = re.Matches(tmpTxt);
+
+                foreach (Match m in mt)
+                {
+                    string sLink = m.ToString();
+                    if (sLink.IndexOf("href") > -1 && (sLink.IndexOf("doc") > -1 || sLink.IndexOf("xml") > -1))
+                    {
+                        string sHref = re2.Match(sLink).Groups[1].ToString();
+                        string sName = re3.Match(sLink).Groups[1].ToString();
+                        arFiles.Add(sHref);
+                    }
+                }
+
+                foreach (string sFile in arFiles)
+                {
+                    if (!File.Exists(copyTo.Text + "//" + sFile))
+                    {
+                        bAllFilesDownloaded = false;
+                        DownloadFile(copyTo.Text + "//"+sFile, sFile);
+                        Thread.Sleep(1000);
+                    }
+                    else
+                    {
+                        Console.WriteLine(sFile + " exists");
+                    }
+                }
+                if (bAllFilesDownloaded)
+                {
+                    MessageBox.Show("שתי הספריות נמצאו זהות");
+                }
+                else
+                {
+                    MessageBox.Show("תהליך ההשוואה והשלמת החסרים הסתיים בהצלחה");
+                }
+            }
+            if (!bContinue)
+            {
+                MessageBox.Show("תהליך ההשוואה נכשל בשל אחת משתי סיבות אפשריות: הספריה המקומית אינה קיימת או שטווח התאריכים שנבחר גדול מיום אחד");
+            }
+
+        }
+    }
+
+
+}
+
+
